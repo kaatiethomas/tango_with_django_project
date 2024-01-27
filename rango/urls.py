50c9b1ea66e3eae940c85b4bb30829b64ec9ad94
Katie Thomas <2756607t@student.gla.ac.uk>
2024-01-27 20:14:28 +0000

    fourth commit - enf of chap 3

--- rango/urls.py
+++ rango/urls.py
@@ -4,6 +4,6 @@ from rango import views
 app_name = 'rango'
 
 urlpatterns = [
-    path('', views.about, name='about'),
+    path('about/', views.about, name='about'),
     path('', views.index, name='index'),
 ]
