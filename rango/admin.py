206aa22f4484ffddbb4b2367937fe820c31ea2ef
Katie Thomas <2756607t@student.gla.ac.uk>
2024-01-31 23:51:02 +0000

    end of chapter 6

--- rango/admin.py
+++ rango/admin.py
@@ -5,5 +5,8 @@ class PageAdmin(admin.ModelAdmin):
     #determines order on individual pages
     list_display = ('title', 'category',  'url')
 
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
