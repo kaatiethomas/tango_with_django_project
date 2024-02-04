a3b96909e61ddac98f145bd4857adc8935aa588b
Katie Thomas <2756607t@student.gla.ac.uk>
2024-02-04 16:46:31 +0000

    9.5 added userProfiles to database

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from rango.models import Category, Page
+from rango.models import UserProfile
 
 class PageAdmin(admin.ModelAdmin):
     #determines order on individual pages
@@ -10,3 +11,4 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
